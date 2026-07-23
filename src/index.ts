4b88b22f633bc696beadb8ada9dc206b8dbd9ca3
samndambuki <sam.ndambuki08@gmail.com>
2026-07-23 14:33:06 +0300

    worked on function annotation in ts

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,53 @@
-//1. define user interface
+//1. define user interface with id,name,age and getMessage() function which returns a string
 //example usage of the interface
+
+//2. Create Profile Interface with name, profileUrl, isActive
+
+//3. Create a function which transforms UserInterface to ProfileInterface
+
+//4. The ProfileUrl is `/profiles/${name}`
+
+//5. Add an optional parameter isActive. Profile must be active if paramater is not provided
 
 export interface UserInterface {
   id: number;
   name: string;
   age: number;
-  getMethod(): string;
+  getMessage(): string;
 }
 
 const user: UserInterface = {
   id: 1,
   name: "Sam",
   age: 25,
-  getMethod() {
+  getMessage() {
     return `${this.name} is ${this.age} years old`;
   },
 };
+
+export interface ProfileInterface {
+  name: string;
+  profileUrl: string;
+  isActive?: boolean;
+}
+
+const transformUserToProfile = (
+  user: UserInterface,
+  isActive: boolean = true,
+): ProfileInterface => {
+  return {
+    name: user.name,
+    profileUrl: `/profiles/${user.name}`,
+    isActive,
+  };
+};
+
+transformUserToProfile(
+  {
+    id: 1,
+    name: "sam",
+    age: 25,
+    getMessage: () => "foo",
+  },
+  false,
+);
